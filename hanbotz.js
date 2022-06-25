775562c14f064d54d74f662e96c3698401b4e703
hanz saja <rintikhujansalju@gmail.com>
2022-06-26 02:24:10 +0700

    make it better

--- hanbotz.js
+++ hanbotz.js
@@ -112,7 +112,7 @@ module.exports = hanbotz = async (hanbotz, m, chatUpdate, store) => {
 
         // Push Message To Console && Auto Read
         if (m.message) {
-            hanbotz.sendReadReceipt([m.key.id])
+            hanbotz.sendReadReceipt(m.chat, m.sender, [m.key.id])
             console.log(chalk.black(chalk.bgWhite('[ PESAN ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> Dari'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> Di'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
        
